e567b836bcf46d02c85f36d80f90434ca3a0b4e9
Rachman Aldiansyah <rachmanaldiansyah03@gmail.com>
2020-10-16 15:56:33 +0700

    Upload Project

---
+++ src/main/java/com/rsoftware/if1/latihan9/IF110119038Latihan9.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rsoftware.if1.latihan9;
+
+/**
+ *
+ * @author
+ * NAMA  : Rachman Aldiansyah
+ * KELAS : IF1
+ * NIM   : 10119038
+ * Deskripsi Program : program ini menampilkan teks ke layar
+ * 
+ */
+
+public class IF110119038Latihan9 {
+    
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        
+        c = 'A';
+        
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+}
